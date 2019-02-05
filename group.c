e3b19fee9ce3efbe52519b69e4a7ed88d72729b3
“[Max <[maxbrand99@gmail.com]>
2019-02-05 14:42:45 -0700

    m

---
+++ group.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void member1(void){
+}
+void member2(void){
+}
+void member3(void){
+}
+void member4(void){
+}
+void member5(void){
+}
+
+int main(void){
+	printf(\n----------------------------------\n");
+	printf(\nOur Group is called: \n");
+	printf(\nGroup Members:\n");
+	printf(\nShawn Bingham\n");
+	printf(\nMax Brandwine\n");
+	printf(\nCole Brooks\n");
+	printf(\nNathan Burkhart\n");
+	printf(\nKevin Christensen\n");
+	printf(\n----------------------------------\n");
+	
+	member1();
+	member2();
+	member3();
+	member4();
+	member5();
+   
+   return 0;  
+}
